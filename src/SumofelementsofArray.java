c9161cbadcc7e70ab8d5af0882641f53f5e8e75a
mkottala <mkottala@iupui.edu>
2018-08-20 17:47:53 -0700

    Sum of elements of Array

---
+++ src/SumofelementsofArray.java
@@ -0,0 +1,12 @@
+public class SumofelementsofArray {
+    public static void main(String[] args) {
+        int[] ar = {1,2,3,4};
+        int sum = 0;
+        for (int i: ar
+             ) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
