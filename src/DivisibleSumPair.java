3bce51e60207e305efa3f291dc8b9ca336d60a24
mkottala <mkottala@iupui.edu>
2018-08-22 22:31:34 -0700

    Divisible Sum Pair

--- src/DivisibleSumPair.java
+++ src/DivisibleSumPair.java
@@ -10,13 +10,11 @@ public class DivisibleSumPair {
     static int divisibleSumPairs(int n, int k, int[] ar) {
         int count = 0;
         int sum = 0;
-        for(int i=0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(ar[i] < ar[j] && i != j){
-                    sum = ar[i] + ar[j];
-                    if (sum % k == 0){
-                        count++;
-                    }
+        for(int i=0; i < n; i++){
+            for(int j = i+1; j <n; j++){
+                sum = ar[i] + ar[j];
+                if (sum % k == 0){
+                    count = count+1;
                 }
             }
         }
@@ -24,9 +22,9 @@ public class DivisibleSumPair {
     }
 
     public static void main(String[] args) {
-        int[] ar ={1,2,3,4,5,6};
-        int n = 6;
-        int k = 5;
+        int[] ar ={71, 44, 2, 93, 66, 27, 41, 99, 49, 68, 60, 16, 45, 21, 71, 96, 89, 91, 60, 21, 43, 9, 56, 48, 25, 96, 91, 99, 73, 22, 48, 32, 27, 71, 72, 90, 9, 62, 68, 70, 77, 98, 2, 32, 69, 51, 99, 35, 47, 83, 82, 43, 87, 47, 40, 54, 53, 85, 78, 31, 98, 26, 56, 100, 88, 43, 77, 81, 58, 31, 46, 70, 57, 8, 16, 53, 8, 61, 22, 62, 75, 94, 91, 29, 95, 69, 22, 12, 88, 5, 87, 90, 10, 86, 86, 85, 73, 95, 87, 53};
+        int n = 100;
+        int k = 22;
         System.out.println(divisibleSumPairs(n,k,ar));
     }
 }
