606a6412559a4b47bb181536cf2dcd3a943ce0c3
mkottala <mkottala@iupui.edu>
2018-08-22 21:14:03 -0700

    Sliding window type problem

---
+++ src/ChocolateSegmentSlidingWindow.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class ChocolateSegmentSlidingWindow {
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+        int count = 0;
+        int temp_sum = 0;
+        for(int i=0; i< m; i++){
+            temp_sum += s.get(i);
+        }
+
+        if(temp_sum == d){
+            count++;
+        }
+
+        for (int i= m ; i< s.size() - 2; i++){
+            temp_sum += s.get(i) - s.get(i-m);
+            if(temp_sum == d){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> chocolateList = new ArrayList<>();
+        chocolateList.add(1);
+        chocolateList.add(2);
+        chocolateList.add(1);
+        chocolateList.add(2);
+        chocolateList.add(3);
+        int result = birthday(chocolateList,3,2);
+        System.out.println(result);
+
+    }
+}
