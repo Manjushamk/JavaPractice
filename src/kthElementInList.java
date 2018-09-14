d7c552efc9ac329b7d9c8aadc720f05eddf1c3f6
mkottala <mkottala@iupui.edu>
2018-09-13 17:22:59 -0700

    CTCI Quesions

--- src/kthElementInList.java
+++ src/kthElementInList.java
@@ -49,21 +49,60 @@ public class kthElementInList {
         return true;
     }
 
+    private static Node partition(Node head, int k) {
+        Node list1 = null;
+        Node temp1 = null;
+        Node temp2 = null;
+        Node list2 = null;
+        Node next = null;
+        Node current = head;
+        while(current != null){
+            if(current.data < k){
+                next = current.next;
+                current.next = null;
+                if(list1 == null){
+                    list1 = current;
+                    temp1 = list1;
+                }
+                temp1.next = current;
+                temp1 = temp1.next;
+                current = next;
+            }else{
+                next = current.next;
+                current.next = null;
+                if(list2 == null){
+                    list2 = current;
+                    temp2 = list1;
+                }
+                temp2.next = current;
+                temp2 = temp2.next;
+                current = next;
+            }
+        }
+        temp1.next = list2;
+        temp2.next = null;
+        return list1;
+    }
+
 
     public static void main(String[] args) {
         kthElementInList list = new kthElementInList();
-        list.head = new Node(1);
-        list.head.next = new Node(2);
-        list.head.next.next = new Node(3);
-        list.head.next.next.next = new Node(4);
-        list.head.next.next.next.next = new Node(5);
-        list.head.next.next.next.next.next = new Node(6);
+        list.head = new Node(3);
+        list.head.next = new Node(5);
+        list.head.next.next = new Node(8);
+        list.head.next.next.next = new Node(5);
+        list.head.next.next.next.next = new Node(10);
+        list.head.next.next.next.next.next = new Node(2);
         int k = 3;
         Node mid = list.head.next.next.next;
         System.out.println(list.getKthElement(head, k));
+        System.out.println();
+        list.printlist(partition(head,5));
+
         System.out.println(deleteMiddle(mid));
         list.printlist(head);
     }
+
 
 }
 
